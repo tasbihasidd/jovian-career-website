d16a025bd6cb848ba1c13645fa2d1c569577caef
tasbihasidd <139063859+tasbihasidd@users.noreply.github.com>
2023-12-25 23:11:45 +0000

    added html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask, render_template
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello, Tasbiha'
+    return render_template("home.html")
 # main driver function
 if __name__ == '__main__':
 
